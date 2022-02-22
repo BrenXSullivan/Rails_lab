8f5e1effdd93b18e4c6dea7da15a5f4560149c1a
m226360 <m226360@usna.edu>
2022-02-22 11:25:26 -0500

    bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
 root "articles#index"
 
-  get "/articles", to: "articles#index"
-  get "/article/:id", to: "articles#show"
+
+resources :articles do
+  resources :comments
+end
+
+
+  # get "/articles", to: "articles#index"
+  # get "/article/:id", to: "articles#show"
 
 end
